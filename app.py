fce7c5821e21e247c5368b4b321790c26eebcfd6
oruiz <oscar98ruiz@gmail.com>
2024-10-15 14:20:00 +0200

    ultra simple json logger

--- app.py
+++ app.py
@@ -2,22 +2,29 @@ from flask import Flask
 from flask import render_template
 import logging
 import logging.handlers
+from pythonjsonlogger import jsonlogger
 
 app = Flask(__name__)
 
 # Logger
 app_logger = logging.getLogger(__name__)
 logger_format= "%(asctime)s: %(levelname)s - PID=%(process)d | %(message)s"
-logging.basicConfig(filename='log', level=logging.DEBUG, format = logger_format)
+logger_format_json= '%(asctime)s %(levelname)s %(process)d %(message)s'
+logging.basicConfig(filename='log', level=logging.DEBUG, format = logger_format_json)
 
 # Disable werkzeug logger to hide startup and HTTP requests
 werkzeug_logger = logging.getLogger("werkzeug")
 werkzeug_logger.disabled = True
 
-# Log Handler (separate logs in files)
+# Log Handler (separate logs in files depending on time)
 handler = logging.handlers.TimedRotatingFileHandler("log", when="S", interval=5, backupCount=10, encoding=None, delay=False, utc=False, atTime=None)
-handler.setFormatter(logging.Formatter(logger_format))
+##handler.setFormatter(logging.Formatter(logger_format))
+# JSON formatter (Comment previous formatter lines)
+json_formatter = jsonlogger.JsonFormatter(logger_format_json)
+handler.setFormatter(json_formatter)
 app_logger.addHandler(handler)
+
+
 
 @app.route("/")
 def index():
